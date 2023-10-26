a25b2a9212c6a59acdd48e88f17940d71fc43255
condebyte <manuelvillamar97@gmail.com>
2023-10-26 20:44:29 +0200

    26/10/23 iniciado la creacion de borrado titulos

--- app/src/main/java/com/example/prueba/data/CarDataSource.kt
+++ app/src/main/java/com/example/prueba/data/CarDataSource.kt
@@ -6,6 +6,9 @@ object CarDataSource {
         cars.add(Car("3456DFC", "BMW", "320CD"))
         cars.add(Car("2314FCT", "BMW", "320CD"))
         cars.add(Car("2967TRW", "BMW", "320CD"))
+        cars.add(Car("3456DFC", "BMW", "320CD"))
+        cars.add(Car("2314FCT", "BMW", "320CD"))
+        cars.add(Car("2967TRW", "BMW", "320CD"))
 
     }
     fun getCars(): ArrayList<Car> {
